da142997a6d56aa1feb96d4c4066478340e601f4
Andrey <andy523451@gmail.com>
2024-05-29 19:10:55 +0300

    first commit

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -1,10 +1,19 @@
 package org.example;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@SessionAttributes("users")
 public class HelloController {
+    private List<User> users = new ArrayList<>();
 
     @GetMapping("/")
     public String homePage() {
@@ -12,7 +21,28 @@ public class HelloController {
     }
 
     @GetMapping("/registerPage")
-    public String registerPage() {
+    public String showRegisterPage() {
         return "registerPage";
+    }
+
+    @PostMapping("/registerPage")
+    public String register(@RequestParam String firstName,
+                           @RequestParam String lastName,
+                           @RequestParam String email, Model model) {
+        User newUser = new User(firstName, lastName, email);
+        users.add(newUser);
+        model.addAttribute("users", users);
+        return "redirect:/thanksPage";
+    }
+
+    @GetMapping("/thanksPage")
+    public String thanksPage() {
+        return "thanksPage";
+    }
+
+    @GetMapping("/registeredUsersList")
+    public String registeredUsersList(Model model) {
+        model.addAttribute("users", users);
+        return "registeredUsersList";
     }
 }
